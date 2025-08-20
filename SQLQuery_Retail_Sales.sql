a66c4a9c185060863359c763ac3bd1fcc2df30eb
RenginaR25 <rrengina@gmail.com>
2025-08-20 18:48:05 -0400

    Update SQLQuery_Retail_Sales.sql

--- SQLQuery_Retail_Sales.sql
+++ SQLQuery_Retail_Sales.sql
@@ -1,27 +1,28 @@
+---View the dataset
 select *
 from dbo.[SQL - Retail Sales Analysis_utf ]
 
----Varify the number of records 
+---Verify the number of records 
 Select count(*)
 from dbo.[SQL - Retail Sales Analysis_utf ]
 
-----Data Cleaning
-----Check NUll values
+----Data Cleaning----------------------------------------
+----Check NUll values------------------------------------
 select *
 from dbo.[SQL - Retail Sales Analysis_utf ]
 where 
 	transactions_id is NULL
-	or quantiy is NULL
+	or quantity is NULL
 
-----Delete records with NUll values
+----Delete records with NUll values----------------------------------
 Delete 
 from dbo.[SQL - Retail Sales Analysis_utf ]
 where 
 	transactions_id is NULL
-	or quantiy is NULL
+	or quantity is NULL
 
-----Data Exploration 
-----Caculate total sale value
+----Data Exploration -----------------------------------------------
+----Caculate total sale value--------------------------
 Select sum(total_sale) as TotalSales
 from dbo.[SQL - Retail Sales Analysis_utf ]
 
@@ -29,7 +30,7 @@ from dbo.[SQL - Retail Sales Analysis_utf ]
 Select count(distinct(customer_id)) as Total_Customer
 from dbo.[SQL - Retail Sales Analysis_utf ]
 
----Number of category and types?
+---Number of categories and types?
 Select count(distinct(category)) as Total_Category
 from dbo.[SQL - Retail Sales Analysis_utf ]
 
@@ -37,59 +38,54 @@ Select distinct(category) as Category_type
 from dbo.[SQL - Retail Sales Analysis_utf ]
 
 ---Data Analysis & Business Key Problem------------------------------------------------------------------- 
----Q1. Write a SQL query to retreive all columns for sales made on 2022-11-05?
----Q2. Write a SQL query to retreive all transactions where the category is clothing and quatity sold is more that 4 in the month of Nov-2022?
----Q3. Write a SQL query to calculate the total sales (total_sale) for each category?
----Q4. Write a SQL query to find the average age of customers who purchased items from the beauty category?
----Q5. Write a SQL query to find all transactions where total_sale is greater than 1000?
----Q6. Write a SQL query to find the total number of transactions made by each gender in each category?
----Q7. Write a SQL query to calculate the average sale for each month. Find out best selling month in each year?
----Q8. Write a SQL query to find the top 5 customers based on the highest total sales?
----Q9. Write a SQL query to find the number of unique customers who purchased items from each category?
----Q10. Write a SQL query to create each shift and number of orders?
+---Q1. Write a SQL query to retrieve all columns for sales made on 2022-11-05.
+---Q2. Write a SQL query to retrieve all transactions where the category is clothing and the quantity sold is more than 4 in the month of Nov-2022?
+---Q3. Write a SQL query to calculate the total sales (total_sale) for each category.
+---Q4. Write a SQL query to find the average age of customers who purchased items from the beauty category.
+---Q5. Write a SQL query to find all transactions where total_sale is greater than 1000.
+---Q6. Write a SQL query to find the total number of transactions made by each gender in each category.
+---Q7. Write a SQL query to calculate the average sale for each month. Find out the best-selling month in each year?
+---Q8. Write a SQL query to find the top 5 customers based on the highest total sales.
+---Q9. Write a SQL query to find the number of unique customers who purchased items from each category.
+---Q10. Write a SQL query to create each shift and the number of orders.
 --------------------------------------------------------------------------------------------------------------
 
----Q1. Write a SQL query to retreive all columns for sales made on 2022-11-05?
+---Q1. Write a SQL query to retrieve all columns for sales made on 2022-11-05.
 Select *
 from dbo.[SQL - Retail Sales Analysis_utf ]
 where sale_date = '2022-11-05'
 
----Q2. Write a SQL query to retreive all transactions where the category is clothing and quatity sold is more that 4 in the month of Nov-2022
+---Q2. Write a SQL query to retrieve all transactions where the category is clothing and the quantity sold is more than 4 in the month of Nov-2022
 Select * 
 from dbo.[SQL - Retail Sales Analysis_utf ]
 where category = 'Clothing'
 	and (CAST(DATEPART(YEAR, sale_date) AS VARCHAR(4)) + '-' +
 		RIGHT('0' + CAST(DATEPART(MONTH, sale_date) AS VARCHAR(2)), 2)) = '2022-11'
-	and quantiy >= 4
+	and quantity >= 4
 
----Q3. Write a SQL query to calculate the total sales (total_sale) for each category?
+---Q3. Write a SQL query to calculate the total sales (total_sale) for each category.
 Select category, sum(total_sale) as TotalSalesByCategory
 from dbo.[SQL - Retail Sales Analysis_utf ]
 group by category
 
----Q4. Write a SQL query to calculate the total sales (total_sale) for each category?
-Select category, sum(total_sale) as TotalSalesByCategory, count(quantiy) as Total_orders
-from dbo.[SQL - Retail Sales Analysis_utf ]
-group by category
-
----Q4. Write a SQL query to find the average age of customers who purchased items from the beauty category?
+---Q4. Write a SQL query to find the average age of customers who purchased items from the beauty category.
 Select AVG(age) as Average_age
 from dbo.[SQL - Retail Sales Analysis_utf ]
 where category = 'Beauty'
 and age is not null
 
----Q5. Write a SQL query to find all transactions where total_sale is greater than 1000?
+---Q5. Write a SQL query to find all transactions where total_sale is greater than 1000.
 Select *
 from dbo.[SQL - Retail Sales Analysis_utf ]
 where total_sale > 1000
 
----Q6. Write a SQL query to find the total number of transactions made by each gender in each category?
+---Q6. Write a SQL query to find the total number of transactions made by each gender in each category.
 Select category, gender, count(transactions_id) as TotalTransaction
 from dbo.[SQL - Retail Sales Analysis_utf ]
 group by category, gender
 order by 1
 
----Q7. Write a SQL query to calculate the average sale for each month. Find out best selling month in each year?
+---Q7. Write a SQL query to calculate the average sale for each month. Find out the best-selling month in each year?
 ----USE CTE
 
 WITH Best_Selling_Month AS(
@@ -103,14 +99,13 @@ WITH Best_Selling_Month AS(
 	GROUP BY
 		YEAR(sale_date),
 		MONTH(sale_date) 
----ORDER BY    1,3 DESC
 )
 
 Select  year_val, month_val, AvgSales
 	FROM Best_Selling_Month 
 	WHERE RankByAvgSale = 1
 
----Q8. Write a SQL query to find the top 5 customers based on the highest total sales?
+---Q8. Write a SQL query to find the top 5 customers based on the highest total sales.
 WITH TOP_5_Customers AS(
 	Select customer_id, sum(total_sale) as TotalSalesByCustomer
 	from dbo.[SQL - Retail Sales Analysis_utf ]
@@ -122,13 +117,13 @@ SELECT TOP 5 customer_id, TotalSalesByCustomer
 from TOP_5_Customers
 order by TotalSalesByCustomer DESC
 
----Q9. Write a SQL query to find the number of unique customers who purchased items from each category?
+---Q9. Write a SQL query to find the number of unique customers who purchased items from each category.
 
 Select category, count(DISTINCT customer_id) as Count_Unique_Customers
 from dbo.[SQL - Retail Sales Analysis_utf ]
 group by category
 
----Q10. Write a SQL query to create each shift and number of orders in each shift?
+---Q10. Write a SQL query to create each shift and the number of orders in each shift.
 WITh Hourly_Sale AS(
 SELECT 
     CASE
